3fa436194e58ea92ecab45411b9841ac3bbbd5b4
unknown <admin@JMJGN-96.dcjmjain.com>
2025-04-17 17:50:11 +0530

    login-register page done but working in email verification

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,7 +12,19 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+    
 
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
+        
+    
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs['placeholder'] = 'First Name'
@@ -21,6 +33,3 @@ class RegistrationForm(forms.ModelForm):
         self.fields['phone_number'].widget.attrs['placeholder'] = 'Phone Number'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
-
-    def clean(self):
-        cleaned_data = super(RegistrationForm)
--- ecart/settings.py
+++ ecart/settings.py
@@ -125,3 +125,16 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+
+#configure email host SMTP
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'sweet007bca@gmail.com'
+EMAIL_HOST_PASSWORD = 'Sannu85888@#'
+EMAIL_USE_TLS = True
