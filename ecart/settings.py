7d766153d94e5f98e76095afc157ef7848121c50
unknown <admin@JMJGN-96.dcjmjain.com>
2025-04-29 18:09:59 +0530

    payment success template done

--- ecart/settings.py
+++ ecart/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Quick-start development settings - unsuitable for production
@@ -137,6 +136,7 @@ MESSAGE_TAGS = {
 #configure email host SMTP
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'demoinfo.in@gmail.com'
-EMAIL_HOST_PASSWORD = 'Sannu85888.'
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'demoinfo.in@gmail.com'
+EMAIL_HOST_PASSWORD = 'dqrwxkyscispiukc'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
--- orders/admin.py
+++ orders/admin.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from .models import Order, OrderProduct, Payment
 
 # Register your models here.
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('payment', 'user', 'product', 'quantity', 'product_price', 'ordered')
+    extra = 0
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('order_number', 'full_name', 'phone', 'email', 'city', 'status', 'is_ordered', 'created_at')
     list_filter = ('status', 'is_ordered')
     search_fields = ('order_number', 'first_name', 'last_name', 'phone', 'email')
     list_per_page = 20
+    inlines = [OrderProductInline]
 
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('payment_id', 'user', 'amount_paid', 'status', 'created_at')
