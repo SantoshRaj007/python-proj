2a66c6a5aebc40cd8a385d584c7cd6c77e9165af
unknown <admin@JMJGN-96.dcjmjain.com>
2025-05-05 17:34:05 +0530

    working on dashboard

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Account
+from .models import Account, UserProfile
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
@@ -31,5 +31,34 @@ class RegistrationForm(forms.ModelForm):
         self.fields['last_name'].widget.attrs['placeholder'] = 'Last Name'
         self.fields['email'].widget.attrs['placeholder'] = 'Enter Email'
         self.fields['phone_number'].widget.attrs['placeholder'] = 'Phone Number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('first_name', 'last_name', 'phone_number')
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Phone Number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+class UserProfileForm(forms.ModelForm):
+    profile_picture = forms.ImageField(required=False, error_messages={'invalid': ("Image files only")}, widget=forms.FileInput)
+    class Meta:
+        model = UserProfile
+        fields = ('address_line_1', 'address_line_2', 'profile_picture', 'city', 'state', 'country', 'zip_code')
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        self.fields['address_line_1'].widget.attrs['placeholder'] = 'Address Line 1'
+        self.fields['address_line_2'].widget.attrs['placeholder'] = 'Address Line 2'
+        self.fields['profile_picture'].widget.attrs['placeholder'] = 'Profile Picture'
+        self.fields['city'].widget.attrs['placeholder'] = 'City'
+        self.fields['state'].widget.attrs['placeholder'] = 'State'
+        self.fields['country'].widget.attrs['placeholder'] = 'Country'
+        self.fields['zip_code'].widget.attrs['placeholder'] = 'Zip Code'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
